7d7909b3019cddbe815f5498c49666a3af2adef3
nkhelifi1 <khelifi.n@live.at>
2023-05-14 22:25:17 +0200

    H2 Datenbank & RESTController

---
+++ src/main/java/htwberlin/webtec/RESTController.java
@@ -0,0 +1,24 @@
+package htwberlin.webtec;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exercises")
+public class RESTController {
+
+    @Autowired
+    private EntityRepository EntityRepository;
+
+    @GetMapping("/api/exercises")
+    public List<ExerciseEntity> getAllExercises() {
+        return EntityRepository.findAll();
+    }
+
+    @PostMapping("/api/exercises")
+    public ExerciseEntity createExercise(@RequestBody ExerciseEntity exercise) {
+        return EntityRepository.save(exercise);
+    }
+}
