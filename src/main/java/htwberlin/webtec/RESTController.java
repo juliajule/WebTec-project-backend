8f53e23d7ed257d7a70935ca6a2c2b4011321437
PuhlmannJulia <s0584638@htw-berlin.de>
2023-05-15 09:53:18 +0200

    Anpassungen im Controller an die Serviceklasse

--- src/main/java/htwberlin/webtec/RESTController.java
+++ src/main/java/htwberlin/webtec/RESTController.java
@@ -1,8 +1,11 @@
 package htwberlin.webtec;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -10,15 +13,23 @@ import java.util.List;
 public class RESTController {
 
     @Autowired
-    private EntityRepository EntityRepository;
+    ExerciseService exerciseService;
 
     @GetMapping("/api/exercises")
-    public List<ExerciseEntity> getAllExercises() {
-        return EntityRepository.findAll();
+    public ResponseEntity<List<Exercise>> fetchExercise() {
+        return ResponseEntity.ok(exerciseService.findAll());
+    }
+
+    @GetMapping("/api/exercise/{id}")
+    public ResponseEntity<Exercise> fetchExerciseById(@PathVariable Long id) {
+        var exercise = exerciseService.findById(id);
+        return exercise != null ? ResponseEntity.ok(exercise) : ResponseEntity.notFound().build();
     }
 
     @PostMapping("/api/exercises")
-    public ExerciseEntity createExercise(@RequestBody ExerciseEntity exercise) {
-        return EntityRepository.save(exercise);
+    public ResponseEntity<Void> createExercise(@RequestBody ExerciseManipulationRequest request) throws URISyntaxException {
+        var exercise = exerciseService.create(request);
+        URI uri = new URI("/api/exercise/" + exercise.getId());
+        return ResponseEntity.created(uri).build();
     }
 }
