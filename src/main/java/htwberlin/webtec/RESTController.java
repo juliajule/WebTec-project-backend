5c5d5f577c6dc28220a6db76593c35d9b070c7a8
PuhlmannJulia <s0584638@htw-berlin.de>
2023-06-24 11:58:11 +0200

    Add Post and Delete

--- src/main/java/htwberlin/webtec/RESTController.java
+++ src/main/java/htwberlin/webtec/RESTController.java
@@ -6,19 +6,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/exercises")
+//@RequestMapping("/api/exercises")
 public class RESTController {
 
     @Autowired
     private EntityRepository EntityRepository;
 
-    @GetMapping
+    @GetMapping("/api/exercises")
     public List<ExerciseEntity> getAllExercises() {
         return EntityRepository.findAll();
     }
 
-    @PostMapping
+    @PostMapping("/api/exercises")
     public ExerciseEntity createExercise(@RequestBody ExerciseEntity exercise) {
         return EntityRepository.save(exercise);
+    }
+
+    @PutMapping("/api/exercises/{id}")
+    public ExerciseEntity updateExercise(@PathVariable long id, @RequestBody ExerciseEntity exercise){
+        if (EntityRepository.findById(id).isEmpty())
+            return null;
+        var exerciseEntity = EntityRepository.findById(id).get();
+        exerciseEntity.setWeight(exercise.getWeight());
+        return EntityRepository.save(exerciseEntity);
+    }
+
+    @DeleteMapping("/api/exercises/{id}")
+    public void deleteExercise(@PathVariable long id){
+        if (!EntityRepository.existsById(id))
+            throw new RuntimeException();
+        EntityRepository.deleteById(id);
     }
 }
