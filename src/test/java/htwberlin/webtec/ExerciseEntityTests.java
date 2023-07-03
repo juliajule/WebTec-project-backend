dd766b07057a58577d254b28e028fb2de4de905a
PuhlmannJulia <s0584638@htw-berlin.de>
2023-07-03 19:42:19 +0200

    Test for ExerciseEntity added

---
+++ src/test/java/htwberlin/webtec/ExerciseEntityTests.java
@@ -0,0 +1,25 @@
+package htwberlin.webtec;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExerciseEntityTests{
+
+    private final ExerciseEntity exerciseEntity = new ExerciseEntity();
+
+    @Test
+    @DisplayName("should return the given Values")
+    public void testExerciseEntity() {
+        exerciseEntity.setName("Bench Press");
+        exerciseEntity.setMuscleGroup("Brust");
+        exerciseEntity.setWeight(40.0);
+        exerciseEntity.setCategory("Push");
+
+        assertEquals("Bench Press", exerciseEntity.getName());
+        assertEquals("Brust", exerciseEntity.getMuscleGroup());
+        assertEquals(40.0, exerciseEntity.getWeight());
+        assertEquals("Push", exerciseEntity.getCategory());
+    }
+
+}
